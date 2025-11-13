cc9ccdd221b1b4d549b8b22be2b12126f762783d
Sbeu <suedis.suedis@gmail.com>
2025-11-13 17:34:55 +0500

    Project structure

--- app/src/main/java/com/sbeu/news/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/sbeu/news/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.sbeu.news.ui.theme
+package com.sbeu.news.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
